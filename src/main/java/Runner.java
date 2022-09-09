2cffa828873b3cf6b2cb605a8122435edaec5f13
Migle Ambrazeviciute <ma320@st-andrews.ac.uk>
2022-09-09 14:14:31 +0300

    pull request process 1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,6 +3,7 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("We're working on develop!");
+        System.out.println("We're making a pull request");
     }
 
 }
